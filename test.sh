d5d6c673e6cbc2a231db4965f01ebdbe2e61ab96
amazingsys <amazingsys@naver.com>
2019-02-12 18:07:35 +0900

    최초 태그만 있고 이후, 태그가 없는 경우에 대한 고민거리 주석 처리

--- test.sh
+++ test.sh
@@ -44,16 +44,6 @@ do
 	fi
 done
 
-#################
-# 로그 관련 소스
-#################
-declare -a log
-index=0
-function recordLog(){
-	local context=$1
-	log[${index}]=${context}
-	let "index+=1"
-}
 
 ##################################
 # today_date: 오늘 날짜
@@ -76,6 +66,31 @@ if [ ! -d ${dir} ]; then
 fi
 cd $project_path
 
+#################
+# 로그 관련 소스
+#################
+declare -a log
+index=0
+function recordLog(){
+	local context=$1
+	log[${index}]=${context}
+	let "index+=1"
+}
+
+function outputLog(){
+	for (( i=0; i<${#log[@]}; i++ ))
+	do
+	    echo "${log[$i]}" >> V:/"${dir}"/log_${today_date}.txt
+	done
+
+	##########################
+	# 파일 생성된 디렉터리 확인
+	##########################
+	cd V:/"${dir}"/
+	start .
+	start log_${today_date}.txt
+}
+
 recordLog "========================$(date '+%Y-%m-%d %H:%M:%S')========================"
 recordLog "!!!!필독!!!!"
 recordLog "태그에는 반드시 브랜치명 '${branch_nm}' 이/가 들어가야합니다. ex) ${branch_nm}_${today_date}_패치"
@@ -83,20 +98,22 @@ recordLog "---------------------------------------------------------------------
 
 # if 이전 태그가 존재하지 않다면, 로그 기록 후, 로직 종료
 # else 이전 태그가 존재한다면, 최종 태그 확인 후, 로직 진행
+if [ -z $(git tag) ]; then
+	echo "태그가 존재하지 않습니다."
+	recordLog "!!!!에러!!!!"
+	recordLog "태그가 존재하지 않습니다."
+	recordLog " "
+
+	outputLog
+	exit
+fi
 if [ -z "${previous_tag}" ]; then
 	recordLog "!!!!에러!!!!"
 	recordLog "이전 태그가 존재하지 않습니다."
 	recordLog "'${branch_nm}_초기셋팅' 태그를 생성해 주십시오."
 	recordLog " "
 
-	for (( i=0; i<${#log[@]}; i++ ))
-	do
-	    echo "${log[$i]}" >> V:/"${dir}"/log_${today_date}.txt
-	done
-
-	cd V:/"${dir}"/
-	start log_${today_date}.txt
-
+	outputLog
 	exit
 fi
 
@@ -113,6 +130,7 @@ name="[${previous_tag}]To[${current_tag}]"
 # if 이전 태그와 최종 태그가 동일한 경우, 최종 태그를 아직 지정하지 않은 상태이므로 공지
 # else 이전 태그와 최종 태그가 동일하지 않은 경우, 공지 없이 정상 작동
 
+#만약 반드시 태그를 만들어야만 만들어지도록 하게끔 한다면 이 부분 수정
 if [ "${previous_tag}" = "${current_tag}" ]; then
 	name="[${previous_tag}]"
 	current_tag="$(git describe --tags)"
@@ -158,16 +176,6 @@ recordLog "---------------------------------------------------------------------
 recordLog " "
 echo "리스트 생성 완료..."
 
-for (( i=0; i<${#log[@]}; i++ ))
-do
-    echo "${log[$i]}" >> V:/"${dir}"/log_${today_date}.txt
-done
-
-##########################
-# 파일 생성된 디렉터리 확인
-##########################
-cd V:/"${dir}"/
-start .
-start log_${today_date}.txt
+outputLog
 #cat log_$(date +%Y)$(date +%m)$(date +%d).txt
 #sleep 3s
