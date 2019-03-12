d23f716e91d4c2be78c8560a9ecfd1af3e46a74c
amazingsys <amazingsys@naver.com>
2019-03-12 13:31:47 +0900

    자동 치환 추가 project_path=$(echo ${temp_project_path}| sed -e "s/\\\/\//g")

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@
 function question(){
 	#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 	Blue='\033[0;35m'
+	Green='\033[0;32m'
 	Color_Off='\033[0m'
 	local context=$1
-	echo -e "${Blue}${context}${Color_Off}"
+	echo -e "${Green}${context}${Color_Off}"
 }
 ###################################
 # 프로젝트 이름 입력/ 실행 위치 이동
@@ -37,8 +38,10 @@ do
 			# if 지정경로가 맞지 않다면, 사용자 직접 경로 설정
 			# else 지정 경로가 맞다면, 지정 경로로 이동
 			if [ "${condition}" != "Y" ] && [ "${condition}" != "y" ]; then
-				question "Q. 이동할 path를 입력해 주세요. '\'가 아닌 '/'로 입력해주세요. ex) V:/"
-				read project_path
+				question "Q. 이동할 path를 입력해 주세요."
+				read -r temp_project_path
+				project_path=$(echo ${temp_project_path}| sed -e "s/\\\/\//g")
+
 				question "Q. '${project_path}' 경로가 확실합니까? Answer: Y/N"
 				read condition
 
@@ -89,7 +92,9 @@ function recordLog(){
 	log[${index}]=${context}
 	let "index+=1"
 }
-
+#################
+# 결과 관련 함수
+#################
 function outputLog(){
 	for (( i=0; i<${#log[@]}; i++ ))
 	do
@@ -98,6 +103,7 @@ function outputLog(){
 
 	##########################
 	# 파일 생성된 디렉터리 확인
+	# 압축된 파일 풀기
 	##########################
 	cd V:/"${dir}"/
 	unzip htdocs_Update_${name}.zip
@@ -106,13 +112,14 @@ function outputLog(){
 }
 name="${branch_nm}_${today_date}_패치"
 recordLog "========================$(date '+%Y-%m-%d %H:%M:%S')========================"
-recordLog "!!!!필독!!!!"
-recordLog "태그에는 반드시 브랜치명 '${branch_nm}' 이/가 들어가야합니다. ex) ${name}"
+#recordLog "!!!!필독!!!!"
+recordLog "태그에는 브랜치명 '${branch_nm}' 이/가 들어가야합니다. ex) ${name}"
 recordLog "--------------------------------------------------------------------------------------------------"
 
+# if 태그가 존재하지 않다면, 로그 기록 후, 로직 종료
 # if 이전 태그가 존재하지 않다면, 로그 기록 후, 로직 종료
-# else 이전 태그가 존재한다면, 최종 태그 확인 후, 로직 진행
-if [ -z $(git tag) ]; then
+# if 최종 태그가 최종 커밋내역인 경우, 로그 기록 후, 로직 종료
+if [ -z $(git tag -l *${branch_nm}*) ]; then # TODO: 현재 브랜치에서의 tag 존재 유무 판단해야함
 	echo "태그가 존재하지 않습니다."
 	recordLog "!!!!에러!!!!"
 	recordLog "태그가 존재하지 않습니다."
@@ -125,6 +132,22 @@ if [ -z "${previous_tag}" ]; then
 	recordLog "!!!!에러!!!!"
 	recordLog "이전 태그가 존재하지 않습니다."
 	recordLog "'${branch_nm}_초기셋팅' 태그를 생성해 주십시오."
+	recordLog " "
+
+	outputLog
+	exit
+fi
+###############################지울곳
+# last_commit: 최종 커밋
+# ex) last commit: 현대해상_20190214_패치-6-g32e9253b7
+# ex) previous_tag: 현대해상_20190214_패치
+###############################
+last_commit="$(git describe --tags HEAD)"
+
+if [ "${previous_tag}" = "${last_commit}" ]; then
+	recordLog "!!!!에러!!!!"
+	recordLog "마지막 태그 이후 커밋된 이력이 없습니다."
+	recordLog "최종 커밋: ${last_commit}"
 	recordLog " "
 
 	outputLog
@@ -152,7 +175,7 @@ fi
 	#recordLog "--------------------------------------------------------------------------------------------------"
 
 	echo "범위 : ${previous_tag} ~ 최종 커밋"
-	recordLog "* 태그 범위 : ${previous_tag} ~ 최종 커밋"
+	recordLog "* 패치 범위 : ${previous_tag} ~ 최종 커밋"
 #else
 	#echo "범위 : ${previous_tag} ~ ${current_tag}"
 	#recordLog "* 태그 범위 : ${previous_tag} ~ ${current_tag}"
@@ -175,7 +198,8 @@ echo "파일 생성 완료..."
 ######################
 echo "리스트 생성 중..."
 #git diff --diff-filter=AMR --name-status ${previous_tag}..${current_tag} -- ':!*etc/*' > V:/"${dir}"/htdocs_Update_list_"${today_date}".txt
-(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*') | sed -e "s/\//\\\/g" > V:/"${dir}"/htdocs_Update_list_"${today_date}".txt
+#(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*') | sed -e "s/\//\\\/g" > V:/"${dir}"/htdocs_Update_list_"${today_date}".txt
+(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*') | sed -e "s/\//\\\/g" > V:/"${dir}"/htdocs_Update_list.txt
 recordLog "- htdocs_Update_list.txt"
 
 ##########################
@@ -184,7 +208,9 @@ recordLog "- htdocs_Update_list.txt"
 #if [ -n "$(git diff --diff-filter=AMR --name-status ${previous_tag}..${current_tag} -- ':!*etc/*' '*.java')" ]; then
 # git diff --diff-filter=AMR --name-status ${previous_tag}..${current_tag} -- ':!*etc/*' '*.java' > V:/"${dir}"/update_java_list_"${name}".txt
 if [ -n "$(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*' '*.java')" ]; then
-	(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*' '*.java') | sed -e "s/\//\\\/g" | sed -e "s/java/class/g" | sed -e "s/src/classes/g" > V:/"${dir}"/htdocs_Update_java_list_"${today_date}".txt
+	#(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*' '*.java') | sed -e "s/\//\\\/g" | sed -e "s/java/class/g" | sed -e "s/src/classes/g" > V:/"${dir}"/htdocs_Update_java_list_"${today_date}".txt
+	(git diff --diff-filter=AMR --name-only ${previous_tag} -- ':!*etc/*' '*.java') | sed -e "s/\//\\\/g" | sed -e "s/java/class/g" | sed -e "s/src/classes/g" > V:/"${dir}"/htdocs_Update_class_list.txt
+
 	recordLog "- htdocs_Update_class_list.txt (!!!!필독!!!! class 파일 필요)"
 fi
 recordLog "- readMe.txt"
